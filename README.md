7d2490927632b427826a32cf0238b19afcf24393
tulihua <tulihua@haitun.hk>
2018-11-12 15:25:01 +0800

    发布基于vue2的json数据转成Excel文件插件

---
+++ README.md
@@ -0,0 +1,101 @@
+# vue-export-excel
+
+> 基于vue2的json数据转成Excel文件插件
+> 优势：
+> 1、逐页从后端拉取数据，对服务器压力很小
+> 2、转换全在客户端完成，速度非常快
+> 3、可转换条数支持百万级别，理论上无上限
+> 4、转换有百分比进度提示
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+# 使用方法：
+### 第一种：页面引入链接方式
+```
+<div id="app">
+    <button onclick="exportExcel()" id="toExcel" class="btn btn-default">导出</button>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script src="exportExcel/exportExcel.js"></script>
+<script>
+
+    function exportExcel() {
+        (new Vue).$exportExcel({
+            target: 'toExcel', //必须, 触发导出的DOM元素ID
+            columns: [
+                { 'label':'订单号', 'prop': 'erp_orders_sn' },
+                { 'label':'订单sku', 'prop': 'orders_sku' },
+                { 'label':'产品名称', 'prop': 'sku_name' },
+                { 'label':'品牌', 'prop': 'brand' },
+                { 'label':'净重', 'prop': 'net_weight' },
+                { 'label':'毛重', 'prop': 'gross_weight' },
+                { 'label':'商品条码', 'prop': 'code' },
+                { 'label':'更新时间', 'prop': 'update_time' }
+            ], //必须, 列头与字段名映射  如 [{'label':'用户名', 'prop': 'username'},{'label':'角色','prop': 'role'}]
+            url: 'http://www.xxxxxx.com/api/Table/index' //后端请求数据地址， 必须
+            page: 'page', //请求分页的参数名称，默认为page, 可选
+            parames: [], //post参数，搜索表单，默认为[], 可选
+            pageSize: 50, //每次拉取数据条数，根据服务器响应速度确定， 默认50, 可选
+            fileName: '',//保存的文件名称, 默认为当前日期， 可选
+            fileType: 'xlsx' //保存的文件类型， 支持的类型  xlsx, csv, txt, xml，  默认为 xlsx, 可选
+        })
+    }
+</script>
+```
+### 第二种： Vue项目中使用
+#### 先在命令行安装
+npm install vue-export-excel -S
+
+```
+<template>
+    <div id="app">
+        <button @click="exportExcel">导出 {{percentage}}</button>
+    </div>
+</template>
+
+<script>
+var app = new Vue({
+        el: '#app',
+        data: {
+            percentage: 0
+        },
+        methods:{
+            exportExcel: function () {
+                this.$exportExcel({
+                      target: 'toExcel', //必须, 触发导出的DOM元素ID
+                      columns: [
+                          { 'label':'订单号', 'prop': 'erp_orders_sn' },
+                          { 'label':'订单sku', 'prop': 'orders_sku' },
+                          { 'label':'产品名称', 'prop': 'sku_name' },
+                          { 'label':'品牌', 'prop': 'brand' },
+                          { 'label':'净重', 'prop': 'net_weight' },
+                          { 'label':'毛重', 'prop': 'gross_weight' },
+                          { 'label':'商品条码', 'prop': 'code' },
+                          { 'label':'更新时间', 'prop': 'update_time' }
+                      ], //必须, 列头与字段名映射  如 [{'label':'用户名', 'prop': 'username'},{'label':'角色','prop': 'role'}]
+                      url: 'http://www.xxxxxx.com/api/Table/index' //后端请求数据地址， 必须
+                      page: 'page', //请求分页的参数名称，默认为page, 可选
+                      parames: [], //post参数，搜索表单，默认为[], 可选
+                      pageSize: 50, //每次拉取数据条数，根据服务器响应速度确定， 默认50, 可选
+                      fileName: '',//保存的文件名称, 默认为当前日期， 可选
+                      fileType: 'xlsx' //保存的文件类型， 支持的类型  xlsx, csv, txt, xml，  默认为 xlsx, 可选
+                  })
+            }
+        }
+    })
+
+</script>
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/lib/exportExcel.js
@@ -0,0 +1,121 @@
+/**
+ * 用法：
+ <div id="app">
+ <button onclick="exportExcel()" id="toExcel" class="btn btn-default">导出</button>
+ </div>
+
+ <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+ <script src="exportExcel/exportExcel.js"></script>
+ <script>
+
+ function exportExcel() {
+        (new Vue).$exportExcel({
+            target: 'toExcel', //必须, 触发导出的DOM元素ID
+            columns: [], //必须, 列头与字段名映射  如 [{'label':'用户名', 'prop': 'username'},{'label':'角色','prop': 'role'}]
+            url: 'http://www.b2b.com/api/Table/index', //后端请求数据地址， 必须
+            page: 'page', //请求分页的参数名称，默认为page, 可选
+            parames: [], //post参数，搜索表单，默认为[], 可选
+            pageSize: 50, //每次拉取数据条数，根据服务器响应速度确定， 默认50, 可选
+            fileName: '',//保存的文件名称, 默认为当前日期， 可选
+            fileType: 'xlsx' //保存的文件类型， 支持的类型  xlsx, csv, txt, xml，  默认为 xlsx, 可选
+        })
+    }
+
+ </script>
+ */
+
+
+import axios from 'axios'
+import jsonExport from './jsonExport'
+import 'element-ui/lib/theme-chalk/progress.css'
+
+let exportData = [];
+let targetBtnTxt = '';
+let targetObj = {};
+
+//拉取要导出的数据的回调
+let getExportData = function(response,currentPage,data) {
+    response.data.data.forEach(function (val, index) {
+        let item = []
+        //将下载的字段名替换成表格的表头名称
+        data.columns.forEach(function (v,k) {
+            //只下载显示的列
+            if(val[v['prop']] != undefined){
+                item[v['label']] = val[v['prop']]
+            }
+        });
+        exportData.push(item)
+    })
+
+    let totalPages = Math.ceil(response.data.total / data.pageSize)
+
+    let percentage = Math.ceil(currentPage / totalPages * 100)
+
+    //全部拉取完后，开始下载数据
+    if(currentPage == totalPages){
+        jsonExport(exportData, data.fileType, data.fileName);
+        targetObj.innerText = targetBtnTxt;
+        targetObj.removeAttribute('disabled');
+    }else{
+        targetObj.innerText = '正在导出，已完成 '+ percentage +'%';
+        targetObj.setAttribute('disabled',true);
+        currentPage++;
+        downloadExport(data,currentPage);
+    }
+
+}
+
+
+//下载导出
+let downloadExport = function (data,currentPage) {
+    axios.post(data.url + '?page='+currentPage, data.parames)
+        .then(function (response) {
+            getExportData(response,currentPage,data)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+const exportExcelPlugin = {
+    install: function(Vue) {
+        Vue.prototype.$exportExcel = function (data) {
+            if(data == null || data == '' || data == undefined){
+                alert('参数缺失！')
+                return false;
+            }
+
+            if(data.url == undefined || data.url == ''){
+                alert('缺少参数url')
+                return false;
+            }
+
+            if(data.columns == undefined || data.columns == '' || data.columns.length == 0){
+                alert('缺少参数columns')
+                return false;
+            }
+
+            if(data.target == undefined || data.target == ''){
+                alert('缺少参数target')
+                return false;
+            }
+
+            if(data.page == undefined || data.page == '') data.page = 'page'
+            if(data.parames == undefined || data.parames == '') data.parames = []
+            if(data.pageSize == undefined || data.pageSize == '') data.pageSize = 50
+            if(data.fileName == undefined || data.fileName == '') data.fileName = ''
+            if(data.fileType == undefined || data.fileType == '') data.fileType = 'xlsx'
+
+            targetObj = document.getElementById(data.target);
+            targetBtnTxt = targetObj.innerText;
+            downloadExport(data,1)
+        }
+    }
+}
+
+// global 情况下 自动安装
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(exportExcelPlugin)
+}
+// 导出模块
+export default exportExcelPlugin
